3c8a962aabe8b8103056ec11532d1e072e6c2e46
Jordan Armstrong <ramblingjordan@gmail.com>
2018-09-09 12:25:50 -0400

    added __init__ files

--- cli/markdone.py
+++ cli/markdone.py
@@ -1,12 +1,13 @@
 import click
 import os.path
 import helpers
+import cmd
 
 @click.command()
 @click.argument('dir', default='.')
 def cli(dir):
 	"""Markdone CLI"""
-	helpers.get_root(dir)
+	root_dir = helpers.get_root(dir)
 
 if __name__ == '__main__':
 	# pylint: disable=no-value-for-parameter
